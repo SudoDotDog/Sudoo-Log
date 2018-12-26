3e7d5b862c2025367d773e3b52670607a932a7c2
WMXPY <wm@mengw.io>
2018-12-26 14:10:50 -0600

    implment data string padding

--- src/util.ts
+++ src/util.ts
@@ -4,6 +4,18 @@
  * @description Util
  */
 
+const padZero = (value: number, digits: number): string => {
+
+    const parsed: string = value.toString();
+    const difference: number = digits - parsed.length;
+    if (difference > 0) {
+
+        return '0'.repeat(difference) + parsed;
+    }
+
+    return parsed;
+};
+
 export const appropriateDateStringWithTime = (date: Date) => {
 
     const year: number = date.getFullYear();
@@ -11,14 +23,22 @@ export const appropriateDateStringWithTime = (date: Date) => {
     const day: number = date.getDate();
 
     const hour: number = date.getHours();
-    const minute: number = date.getMinutes();
+    const minutes: number = date.getMinutes();
     const seconds: number = date.getSeconds();
 
     const area: number = Math.floor(date.getTimezoneOffset() / 60);
+
+    const parsedYear: string = padZero(year, 4);
+    const parsedMonth: string = padZero(month, 2);
+    const parsedDay: string = padZero(day, 2);
+
+    const parsedHour: string = padZero(hour, 2);
+    const parsedMinutes: string = padZero(minutes, 2);
+    const parsedSeconds: string = padZero(seconds, 2);
 
     const areaStr: string = area >= 0
         ? '+' + area.toString()
         : area.toString();
 
-    return `${year}-${month}-${day} ${hour}:${minute}:${seconds}(UTC${areaStr})`;
+    return `${parsedYear}-${parsedMonth}-${parsedDay} ${parsedHour}:${parsedMinutes}:${parsedSeconds} (UTC${areaStr})`;
 };
